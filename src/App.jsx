8d8718cb2c672e50c2d1e3f9567b805c3600e41e
CodeAssasinKing <bayramowramazan85@gmail.com>
2026-05-19 18:34:59 +0500

    packages are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
-import { useEffect } from 'react'
-import { Routes, Route, useLocation } from 'react-router-dom'
-import Home from './Home'
-import ContactSection from './pages/Contact/ContactSection'
+import { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Home from "./Home";
+import ContactSection from "./pages/Contact/ContactSection";
 
 // Компонент-помощник для сброса скролла
 function ScrollToTop() {
-	const { pathname } = useLocation()
+  const { pathname } = useLocation();
 
-	useEffect(() => {
-		window.scrollTo(0, 0)
-	}, [pathname])
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
-	return null
+  return null;
 }
 
 function App() {
-	return (
-		<div className='bg-zinc-950 min-h-screen text-white'>
-			{/* Теперь при каждом переходе скролл будет прыгать в начало */}
-			<ScrollToTop />
+  return (
+    <div className="bg-zinc-950 min-h-screen text-white">
+      {/* Теперь при каждом переходе скролл будет прыгать в начало */}
+      <ScrollToTop />
 
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/contact' element={<ContactSection />} />
-			</Routes>
-		</div>
-	)
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<ContactSection />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
